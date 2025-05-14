e1c6b097b654da010fca39f099e5602fae8d1b50
jimmy-maison <Jimmy.Maison@hotmail.com>
2025-05-14 09:57:28 -0700

    Change height and width from meta image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,8 +23,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "https://ned-tech-web.pages.dev/img/landing.png",
-        width: 2833,
-        height: 1117,
+        width: 1200,
+        height: 630,
         alt: "NED Stack Preview Image",
       },
     ],
