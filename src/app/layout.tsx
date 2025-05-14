d779fe0f9fda682196064564fbc1002ca02137a2
jimmy-maison <Jimmy.Maison@hotmail.com>
2025-05-14 10:48:51 -0700

    Test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,8 +24,8 @@ export const metadata: Metadata = {
       {
         url: "https://ned-tech-web.pages.dev/img/landing.png",
         alt: "NED Stack Preview Image",
-        width: 1200,         
-        height: 630,
+        width: 1800,         
+        height: 709,
       },
     ],
     locale: "en_US",
